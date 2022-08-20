39431f8f5d2c3b57cdd925e1778bf158edc9a19c
crosspi <mc748159263@outlook.com>
2022-08-21 01:29:51 +0800

    'add translation'

--- history.py
+++ history.py
@@ -2,22 +2,22 @@ import requests
 
 url = "https://api.ahfi.cn/api/lsjt?format=json"
 
-payload={}
+payload = {}
 headers = {
-  'Cache-Control': 'no-cache',
-  'Accept': '*/*',
-  'Accept-Encoding': 'gzip, deflate',
-  'Connection': 'keep-alive'
+    'Cache-Control': 'no-cache',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
-history=response.text
-history=eval(history)
-print('今天是:%s'%history.get('day'))
+history = response.text
+history = eval(history)
+print('今天是:%s' % history.get('day'))
 print('历史上的今天发生了:')
-list1=history.get('content')
-ii=1
+list1 = history.get('content')
+ii = 1
 for i in list1:
-    print('%d.%s'%(ii,i))
-    ii+=1
+    print('%d.%s' % (ii, i))
+    ii += 1
--- selenium1.py
+++ selenium1.py
@@ -3,9 +3,11 @@ from msedge.selenium_tools import Edge, EdgeOptions
 import time
 options = EdgeOptions()
 options.use_chromium = True
-options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" # 浏览器的位置
-driver = Edge(options=options, executable_path=r"C:\Users\86138\AppData\Local\Programs\Python\Python310\msedgedriver.exe") # 相应的浏览器的驱动位置
-driver.set_window_size(1920,1080)
+# 浏览器的位置
+options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+driver = Edge(options=options,
+              executable_path=r"C:\Users\86138\AppData\Local\Programs\Python\Python310\msedgedriver.exe")  # 相应的浏览器的驱动位置
+driver.set_window_size(1920, 1080)
 driver.get('http://www.baidu.com')
 
 driver.find_element_by_id('kw').send_keys('python')
