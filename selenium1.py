996c15a751d20a8f35f64bdd36990487c9750e62
crosspi <mc748159263@outlook.com>
2022-08-25 03:33:33 +0800

    selenium3-->selenium4 add with_tag_name

--- selenium1.py
+++ selenium1.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options as EdgeOptions
 
+# from selenium.webdriver.support.relative_locator import with_tag_name
+
 options = EdgeOptions()
 options.use_chromium = True
 # 浏览器的位置
@@ -17,3 +19,9 @@ element.send_keys("python")
 driver.find_element(By.ID, 'kw').submit()
 time.sleep(2)
 driver.quit()
+# password = driver.find_element(By.ID, "password")  # 通过id定位 密码输入框
+# emailAddress = driver.find_element(with_tag_name("input").above(password))  # 通过相对定位定位邮箱地址输入框
+# password = driver.find_element(with_tag_name("input").below(emailAddress))
+# cancelButton = driver.find_element(with_tag_name("button").to_left_of(submitButton))
+# submitButton = driver.find_element(with_tag_name("button").to_right_of(cancelButton))
+# emailAddressField = driver.find_element(with_tag_name("input").near(emailAddressLabel))
